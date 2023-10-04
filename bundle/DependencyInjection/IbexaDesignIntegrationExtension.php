5e163805065892d1c68268dece73a05b662fae6b
Florian ALEXANDRE <f.alexandre@novactive.com>
2023-10-04 14:38:35 +0200

    feat: generate ibexa image variations from image configuration

--- bundle/DependencyInjection/IbexaDesignIntegrationExtension.php
+++ bundle/DependencyInjection/IbexaDesignIntegrationExtension.php
@@ -37,12 +37,55 @@ class IbexaDesignIntegrationExtension extends Extension implements PrependExtens
 
     public function prepend(ContainerBuilder $container): void
     {
-        if (!$container->hasExtension('twig')) {
+        $this->addTwigConfiguration( $container );
+        $this->addImageVariationConfig( $container );
+    }
+
+
+    protected function addImageVariationConfig(ContainerBuilder $container): void
+    {
+        $variationsConfig = [];
+        $breakpoints = $container->getParameter('erdnaxelaweb.static_fake_design.image.breakpoints');
+        $variations = $container->getParameter('erdnaxelaweb.static_fake_design.image.variations');
+        foreach ( $variations as $variationName => $variationSizes )
+        {
+            foreach ( $variationSizes as $i=>$variationSize )
+            {
+                $breakpoint = $breakpoints[$i];
+                $variationFullName = "{$variationName}_{$breakpoint['suffix']}";
+                $variationsConfig[$variationFullName] = [
+                    'reference' => null,
+                    'filters' => [
+                        ['name' => 'focusedThumbnail', 'params'=> [ 'size' => $variationSize, 'focus' => [0,0]]]
+                    ]
+                ];
+            }
+        }
+
+        $container->prependExtensionConfig(
+            "ibexa",
+            [
+                'system' => [
+                    'site' => [
+                        'image_variations' => $variationsConfig
+                    ]
+                ]
+            ]
+        );
+    }
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     * @return void
+     */
+    private function addTwigConfiguration( ContainerBuilder $container ): void
+    {
+        if ( !$container->hasExtension( 'twig' ) )
+        {
             return;
         }
 
-        $path = __DIR__.'/../Resources/views';
-        $container->prependExtensionConfig('twig', ['paths' => [$path]]);
+        $path = __DIR__ . '/../Resources/views';
+        $container->prependExtensionConfig( 'twig', [ 'paths' => [ $path ] ] );
     }
 
 }
