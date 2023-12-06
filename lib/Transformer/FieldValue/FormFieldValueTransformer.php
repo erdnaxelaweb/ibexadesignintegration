822df92d75108dc1724e7dcc0f814757e4a6a6fc
Florian ALEXANDRE <f.alexandre@novactive.com>
2023-12-06 09:46:43 +0100

    feat: form field generator

--- lib/Transformer/FieldValue/FormFieldValueTransformer.php
+++ lib/Transformer/FieldValue/FormFieldValueTransformer.php
@@ -14,25 +14,24 @@ namespace ErdnaxelaWeb\IbexaDesignIntegration\Transformer\FieldValue;
 use Ibexa\Contracts\Core\Repository\Values\Content\Content;
 use Ibexa\Contracts\Core\Repository\Values\ContentType\FieldDefinition;
 
-class FormFieldValueTransformer  implements FieldValueTransformerInterface
+class FormFieldValueTransformer implements FieldValueTransformerInterface
 {
     public function transformFieldValue(
         Content         $content,
         string          $fieldIdentifier,
         FieldDefinition $fieldDefinition,
         array           $fieldConfiguration
-    )
-    {
+    ) {
         return function ($modelData = null) use ($content, $fieldIdentifier, $fieldDefinition) {
             /** @var \Ibexa\FormBuilder\FieldType\Value $fieldValue */
             $fieldValue = $content->getFieldValue($fieldIdentifier);
 
             $form = $fieldValue->getForm();
-            if($modelData) {
-                $form->get('fields')->setData($modelData);
+            if ($modelData) {
+                $form->get('fields')
+                    ->setData($modelData);
             }
             return $form->createView();
         };
     }
-
 }
